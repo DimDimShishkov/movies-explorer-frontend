d186085b6fb3b122ffc992cb2ba07343302d9445
DimDimShishkov <99988422+DimDimShishkov@users.noreply.github.com>
2022-11-11 22:43:34 +0400

    initial commit

---
+++ src/components/NoPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NoPage() {
+  return (
+    <section className="main">
+      <div className="nopage">
+        <h1 className="nopage__title">404</h1>
+        <h2 className="nopage__subtitle">Страница не найдена.</h2>
+        <button className="nopage__button">Назад</button>
+      </div>
+    </section>
+  );
+}
